426ec1847d2a65ae907314470b04cf1c406478b4
GloriaBargoria <bargoriagloria@gmail.com>
2023-02-02 01:16:12 +0300

    contact us page

--- src/App.js
+++ src/App.js
@@ -4,24 +4,25 @@ import { toast } from "react-toastify";
 
 import "./App.css";
 
-import Landing from './pages/landing';
-import LawyerSignup from './pages/lawyer/signup'
-import ClientSignup from './pages/client/signup'
-import Signin from './pages/lawyer/signin'
-import ForgotPassword from './pages/forgotPassword'
-import ResetPassword from './pages/resetPassword'
-import FourOhFour from './pages/404';
-import ClientDashboard from './pages/client/dashboard'
-import LawyerDashboard from './pages/lawyer/dashboard'
-import Onboarding from './pages/lawyer/onboarding'
-import SplashScreen from './pages/splashScreen'
-import Flutterwave from './pages/lawyer/flutterwave'
+import Landing from "./pages/landing";
+import LawyerSignup from "./pages/lawyer/signup";
+import ClientSignup from "./pages/client/signup";
+import Signin from "./pages/lawyer/signin";
+import ForgotPassword from "./pages/forgotPassword";
+import ResetPassword from "./pages/resetPassword";
+import FourOhFour from "./pages/404";
+import ClientDashboard from "./pages/client/dashboard";
+import LawyerDashboard from "./pages/lawyer/dashboard";
+import Onboarding from "./pages/lawyer/onboarding";
+import SplashScreen from "./pages/splashScreen";
+import Flutterwave from "./pages/lawyer/flutterwave";
 
 import { refresh } from "./data/controller";
 
 import { userStore } from "./stores";
 import Pricing from "./components/landing/Pricing";
 import Example from "./components/landing/Example";
+import ContactUs from "./components/landing/ContactUs";
 
 function App() {
   const user = userStore((state) => state.user);
@@ -31,7 +32,7 @@ function App() {
   const setIsLoadingUser = userStore((state) => state.setIsLoadingUser);
 
   useEffect(() => {
-    console.log("User", user)
+    console.log("User", user);
 
     if (!user) {
       setIsLoadingUser(true);
@@ -61,19 +62,19 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/pricing" element={<Pricing />} />
+        <Route path="/contact" element={<ContactUs />} />
         <Route path="/example" element={<Example />} />
-        <Route path='/lawyer-signup' element={<LawyerSignup />} />
-        <Route path='/client-signup' element={<ClientSignup />} />
-        <Route path='/signin' element={<Signin />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route path='/resetPassword/:token' element={<ResetPassword />} />
-        <Route path='/payments/flutterwave' element={<Flutterwave />} />
-        {
-          user?.role === "client" && (
-            <>
-              <Route path="/dashboard/*" element={<ClientDashboard />} />
-            </>
-          )}
+        <Route path="/lawyer-signup" element={<LawyerSignup />} />
+        <Route path="/client-signup" element={<ClientSignup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/resetPassword/:token" element={<ResetPassword />} />
+        <Route path="/payments/flutterwave" element={<Flutterwave />} />
+        {user?.role === "client" && (
+          <>
+            <Route path="/dashboard/*" element={<ClientDashboard />} />
+          </>
+        )}
         {user?.role === "lawyer" && (
           <>
             <Route path="/onboarding" element={<Onboarding />} />
