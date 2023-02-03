f7da5524e6211a43e66ba05d17e0e96a18157448
GloriaBargoria <bargoriagloria@gmail.com>
2023-02-03 23:48:08 +0300

    version

--- public/worker.js
+++ public/worker.js
@@ -5,7 +5,7 @@ importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
 );
 
-const LATEST_VERSION = "v1.2.0";
+const LATEST_VERSION = "v1.2.1";
 const NAME = "legal-pwa";
 
 workbox.setConfig({ debug: true });
